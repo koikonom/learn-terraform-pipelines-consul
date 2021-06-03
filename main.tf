3f6779e9aaf157a5b0c24316719b7c89be0acb8c
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2021-06-03 15:10:24 +0100

    set workspace name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "kyriakos-o-consul"
     }
   }
   required_providers {
